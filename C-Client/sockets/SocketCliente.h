3b014631089de9542c7741e453e0c5860d09d5f1
Valeria <39573018+valeria-ortiz7@users.noreply.github.com>
2021-06-30 18:08:16 -0600

    [UPDATE] Added constants to header

--- C-Client/sockets/SocketCliente.h
+++ C-Client/sockets/SocketCliente.h
@@ -1,6 +1,9 @@
 #ifndef CLIENTE_SOCKETCLIENTE_H
 #define CLIENTE_SOCKETCLIENTE_H
 
+char *IP_ADDRESS = "127.0.0.1";
+int PORT = 17778;
+
 char* enviar_mensaje(char* mensaje_solicitud);
 
 #endif //CLIENTE_SOCKETCLIENTE_H
